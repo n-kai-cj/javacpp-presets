14bc428538a6e5b21f79f53a901864591e92f4fa
n-kai-cj <n.kai.cj.github@gmail.com>
2020-12-30 18:49:07 +0900

    test5

--- opencv/cppbuild.sh
+++ opencv/cppbuild.sh
@@ -126,7 +126,7 @@ if [[ "$EXTENSION" == *gpu ]]; then
 fi
 
 # exclude openblas dependencies
-echo "+++++@@@@@ test4 opencv/cppbuild.sh platform=${PLATFORM}, NOOPENBLAS=${NOOPENBLAS}"
+echo "+++++@@@@@ test5 opencv/cppbuild.sh platform=${PLATFORM}, NOOPENBLAS=${NOOPENBLAS}"
 if [[ "${NOOPENBLAS:-no}" == "yes" ]]; then
     # arguments: <file name> <exclude key word>
     function del_keyword_line {
